fde9598c2587c5adc2c20022ecc30abc156473df
Anne Howell <annehowell@Annes-MacBook-Pro.local>
2021-07-18 23:14:35 -0400

    made DRYER

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// The purpose of script.js is to implement a chapter viewing carousel that will allow the user
-// to view the chapter title, number, summary, and cover image of all 900+ (and growing) One Piece issues
+// For the arcspage.js, we implemented our pieceApp from script.js but with newer features. To see the comments
+// for pieceApp, please see the script.js file
 
-// This JS file will allow the user to cycle to the previous, and the next chapter at the click of a button
-// whilst staying on the same page, resulting in more efficient viewing and less server load
+// Arcspage.js will display the arcs list, and allow the user to select an arc and begin viewing the chapter
+// at the start of the arc. Once selected, a scrollable menu will appear, allowing the user to cycle through
+// different arcs easily without heading back to the main Arcs page
 
-// Defining our name-spacing for our website
 const pieceApp = {};
 
-// Fetching our API from One Piece Cover, and running it through a proxy courtesy of Juno College
 pieceApp.getChapters = function(){
     
-    const proxiedUrl = `https://onepiececover.com/api/chapters/${pieceApp.currentChapter}`
+    let proxiedUrl = `https://onepiececover.com/api/chapters/${pieceApp.currentChapter}`
 
     const url = new URL('https://proxy.hackeryou.com');
 
@@ -29,9 +28,6 @@ fetch(url)
     }
 
 
-// This display chapter function will implement the visuals of the chapters page
-// It will display the chapter title, number, summary, and cover images
-
 pieceApp.displayChapter = function (arrayOfChapters) {
         
         const title = document.querySelector('.chapter-title');
@@ -49,8 +45,6 @@ pieceApp.displayChapter = function (arrayOfChapters) {
         chapterImage.src = arrayOfChapters.cover_images.split('|')[0] 
 }
 
-// This change chapter function will allow the user to cycle through different chapters
-// Utilizing the prev and next buttons, the chapters will change dynamically whilst staying on the same page
 
 pieceApp.changeChapter = function() {
     const next = document.querySelector('.next');
@@ -61,7 +55,7 @@ pieceApp.changeChapter = function() {
 })
     const previous = document.querySelector('.previous');
     previous.addEventListener('click', function() { 
-        
+
         if (pieceApp.currentChapter !== 1){
         pieceApp.currentChapter = pieceApp.currentChapter - 1;
         pieceApp.getChapters(pieceApp.currentChapter);
@@ -69,26 +63,39 @@ pieceApp.changeChapter = function() {
     })
 }
 
+// Defining the elements used to add functionality for the arcs menu
+const arcMenu = document.querySelector('#arc-menu');
 
-// chSelectors will allow the user to utilize the menu on the left hand side to manually select which of
-// the 900+ chapters they want to visit, loading almost instantly
 
+const chapterGallery = document.querySelector('#hide-chapters');
+const boxes = document.querySelector('.arcs-list')
+
+// Defining the elements used to add functionality for the arcs list
+const firstArc = document.querySelector('#arc-1');
+const secondArc = document.querySelector('#arc-2');
+const thirdArc = document.querySelector('#arc-3');
+const fourthArc = document.querySelector('#arc-4');
+const fifthArc = document.querySelector('#arc-5');
+const sixthArc = document.querySelector('#arc-6');
+const seventhArc = document.querySelector('#arc-7');
+const eighthArc = document.querySelector('#arc-8')
+const ninthArc = document.querySelector('#arc-9');
+const tenthArc = document.querySelector('#arc-10');
+
+
+// The arcSelectors event listener will allow the user to pick an arc from the scrollable menu
 pieceApp.menu = function() {
-    const chSelectors = document.querySelector('#chapter-selection')
-
-    chSelectors.addEventListener("change", function (event) {
+    const menuSelectors = document.querySelector('#menu-selection')
+    menuSelectors.addEventListener("change", function (event) {
         event.preventDefault
         const selection = this.value
-
         pieceApp.currentChapter = 1 * selection
         pieceApp.getChapters(pieceApp.currentChapter)
     })
 }
 
-// The init function that will allow us to run our JS, defaulting the chapter value to 1 whilst grabbing
-// all the 900+ chapters info from the API and displaying it on the page
 
-pieceApp.init = function() {
+pieceApp.init = function () {
     pieceApp.currentChapter = 1
     pieceApp.getChapters(pieceApp.currentChapter)
     pieceApp.changeChapter();
@@ -97,3 +104,115 @@ pieceApp.init = function() {
 
 pieceApp.init();
 
+
+// This function will hide the chapter viewing page & and the scrollable menu until an arc is chosen
+function hideChapters() {
+    chapterGallery.style.display = "none";
+    arcMenu.style.display ="none"
+}
+hideChapters();
+
+// This function will display the chapter viewing page & and the scrollable menu when an arc is chosen
+function showChapters() {
+    chapterGallery.style.display = "block";
+    arcMenu.style.display = "block";
+
+}
+
+// The corresponding event listeners below will allow the user to select an arc and be taken to its respective chapter
+
+firstArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 1
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+
+secondArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 101
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+thirdArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 218
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+
+fourthArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 303
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+fifthArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 442
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+
+sixthArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 490
+    pieceApp.getChapters(pieceApp.currentChapter)
+    
+})
+
+seventhArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 598
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+
+eighthArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 654
+    pieceApp.getChapters(pieceApp.currentChapter) 
+})
+
+ninthArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 802
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
+
+
+tenthArc.addEventListener("click", (event) => {
+    event.preventDefault();
+    showChapters();
+    boxes.style.display = "none";
+
+    pieceApp.currentChapter = 903
+    pieceApp.getChapters(pieceApp.currentChapter)
+})
